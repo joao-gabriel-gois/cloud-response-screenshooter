d6860b236f0aa81365b722ea718eb946e61d783f
Joao Gabriel <gois.jg.dev@gmail.com>
2022-06-24 22:38:03 -0300

    [fix]ci: all tests until now missed the .env file in instance

--- src/infra/server.js
+++ src/infra/server.js
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import { app } from './app.js';
 
-//const S3_BASE_URL = process.env.AWS_BUCKET_URL;
-//console.log(S3_BASE_URL);
+const PORT = process.env;
 
 export default () => {
-  app.listen(process.env.PORT, () => {
-    console.log("[my-cli-browser] Started! Running...");
+  app.listen(PORT, () => {
+    console.log(`[my-cli-browser] Started! ${PORT ? `Running on PORT ${PORT}` : "NO PORT FOUND, ADD .env FILE!"}`);
+    if (!PORT) {
+      console.error("NO .env file, process not running on expected PORT!");
+      throw new Error("NO .env File", "Server could not start because .env file was not added to running instance");
+    }
   });
 }
